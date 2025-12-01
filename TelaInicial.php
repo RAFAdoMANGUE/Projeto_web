35dfa654d7e28bd349da013421d28fbc09b5aae4
viniciusfredrich <vinihfredi@gmail.com>
2025-11-30 22:51:26 -0300

    dale

--- TelaInicial.php
+++ TelaInicial.php
@@ -1,5 +1,35 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class = 'login'>
+        <h1>Jogo de digitação do Mario</h1>
+        <form action="VerificarUsuario.php" method="POST">
+        <h2>Faça seu login para jogar<h2>
+        <br></br>
+        <label>email</label>
+        <input type="email" name="emailUsuario">
+        <label>senha</label>
+        <input type="password" name="senhaUsuario">
+
+        <button type ='submit'>Jogar</button>
+        <button type='button' onclick='FuncChamarRegistro()'>registrar</button>
+        </div>
+
+        <script>
+            function FuncChamarRegistro() {
+                window.location.href='indexprojeto.php';
+            }
+        </script>
+</body>
+</html>
+
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
@@ -31,34 +61,4 @@
   </script>
 </body>
 </html>
-=======
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class = 'login'>
-        <h1>Jogo de digitação do Mario</h1>
-        <form action="VerificarUsuario.php" method="POST">
-        <h2>Faça seu login para jogar<h2>
-        <br></br>
-        <label>email</label>
-        <input type="email" name="emailUsuario">
-        <label>senha</label>
-        <input type="password" name="senhaUsuario">
 
-        <button type ='submit'>Jogar</button>
-        <button type='button' onclick='FuncChamarRegistro()'>registrar</button>
-        </div>
-
-        <script>
-            function FuncChamarRegistro() {
-                window.location.href='indexprojeto.php';
-            }
-        </script>
-</body>
-</html>
->>>>>>> a2a65119e1c314c8f6aac2f02329daaad0ac26cd
