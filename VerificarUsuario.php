93371064bf9aec3b6313dd4de9611d0e629a334a
dancleve <dancleve1999@gmail.com>
2025-11-30 22:30:49 -0300

    segundo commit

---
+++ VerificarUsuario.php
@@ -0,0 +1,21 @@
+<?php
+require 'connection.php';
+
+
+    $jogarEmail = $_POST['emailUsuario'];
+    $jogarSenha = $_POST['senhaUsuario'];
+
+    $sqlConsJogar = 'select email from usuario where email = :email AND senha= :senha';
+
+    $stmtConsJogar = $conn ->prepare($sqlConsJogar);
+    $stmtConsJogar->bindValue(':email', $jogarEmail);
+    $stmtConsJogar->bindValue(':senha', $jogarSenha);
+    $stmtConsJogar->execute();
+    $user= $stmtConsJogar->fetch(PDO::FETCH_ASSOC);
+    if($user){
+         header("Location: index.html");
+         exit;
+    } 
+    else{
+        echo'login invalido';
+    }
---
+++ indexprojeto.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registro</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class ='acesso'>
+  <h1>mario games type's</h1>
+  <form action="salvar.php" method="POST">
+    <br></br>
+    <label>email</label>
+    <input type="email" name="emailUsuario">
+    <label>senha</label>
+    <input type="password" name="senhaUsuario">
+    <button type ='submit'>enviar</button>
+  </form>
+</div>    
+</body>
+</html>
