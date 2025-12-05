f8747a3cfd7cdccb4aaa0873e88888938c04df57
Rafael <RAFADOMANGUE>
2025-12-04 22:25:45 -0300

    retirada botão salvar ponto

--- jogo.js
+++ jogo.js
@@ -3,7 +3,7 @@ let vida = document.getElementById("vidas");
 let intPontos = 0;
 let tempo = 0;
 box.style.position = "absolute"; 
-let intVida = 3; 
+let intVida = 1; 
 
 let boxObjeto = document.getElementById("objetoBomba");
 boxObjeto.style.position = "absolute";
@@ -37,6 +37,7 @@ let palavraCaixa = document.getElementById("palavra");
 
 let somCorrect = new Audio("sound/Correct.wav");
 let music = new Audio("sound/Music.wav");
+let explosion = new Audio("sound/Explosion.wav");
 
 let ultimaPontuacao = 0;
 
@@ -126,6 +127,7 @@ function mover() {
 
     if(intVida <= 0){
         endGame();
+        salvarPontos()
     }
 
     if(resposta == pergunta){
@@ -216,12 +218,12 @@ function ObjectCorrect(){
 
     Bomba.posyO = 0;
     boxObjeto.style.top = "0px";
-
     somCorrect.play();
 }
 
 function endGame() {
     ultimaPontuacao = intPontos;
+    explosion.play();
 
     alert("Fim de jogo! Você fez " + ultimaPontuacao + " pontos.");
 
@@ -253,6 +255,7 @@ function endGame() {
     document.getElementById("objetoBomba").textContent = "teste";
 
     start = true;
+
 }
 
 function salvarPontos() {
@@ -268,6 +271,7 @@ function salvarPontos() {
         },
         body: 'pontos=' + encodeURIComponent(ultimaPontuacao)
     })
+    
     .then(response => response.text())
     .then(text => {
         alert(text); 
